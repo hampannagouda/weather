6bdfbcd0c9791a02368112109f7d500f47630667
hampannagouda <hampannagouda18@gmail.com>
2026-07-15 17:48:33 +0530

    Add make_nws_request function to handle NWS API requests

--- weather.py
+++ weather.py
@@ -1,3 +1,5 @@
+#https://github.com/modelcontextprotocol/quickstart-resources/blob/main/weather-server-python/weather.py
+
 from typing import Any
 
 import httpx
@@ -9,3 +11,4 @@ mcp = FastMCP("weather")
 NWS_API_BASE = "https://api.weather.gov"
 USER_AGENT = "weather-app/1.0"
 
+async def make_nws_request(url: str) -> dict[str, Any]:
